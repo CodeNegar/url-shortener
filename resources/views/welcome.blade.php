606beb315d0e482da1fc2d67cd024e0b1d4fca5f
Farhad Ahmadi <ahm.farhad@gmail.com>
2018-05-14 23:07:04 +0800

    Welcome page optimized, assets added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,16 +12,21 @@
     <link href="https://getbootstrap.com/docs/4.1/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="https://getbootstrap.com/docs/4.1/examples/sign-in/signin.css" rel="stylesheet">
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+
   </head>
 
   <body class="text-center">
-    <form class="form-signin">
+    <form class="form-shorten">
       <h1 class="h3 mb-3 font-weight-normal">Please Enter your URL</h1>
-      <label for="inputEmail" class="sr-only">Long URL</label>
-      <input type="url" id="inputEmail" class="form-control mb-3" placeholder="Long URL" required autofocus>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Shorten</button>
+      <label for="inputURL" class="sr-only">Long URL</label>
+      <input type="url" id="inputURL" class="form-control mb-3" value="http://" name="longurl" id="longurl" placeholder="Long URL" required autofocus>
+      <button class="btn btn-lg btn-primary btn-block" id="shorten-btn" type="submit">Shorten</button>
       <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
     </form>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
+
   </body>
 </html>
