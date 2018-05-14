5a93477ffe2efdff6cac11d8f584c9b7110240dd
Farhad Ahmadi <ahm.farhad@gmail.com>
2018-05-14 22:32:14 +0800

    API and web routes added

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 
+Route::get('/index', 'UrlController@index');
+Route::post('/store', 'UrlController@store');
+Route::get('/{url}/stats', 'UrlController@stats');
 Route::get('/test', function(){
     return [
         'status' => 'success',
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/{url}/go', 'UrlController@show');
+Route::get('/{url}/stats', 'UrlController@show');
