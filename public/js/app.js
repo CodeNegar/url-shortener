cfa77ce91d55a03952dc00a2b712ab3b1cf5aab3
Farhad Ahmadi <ahm.farhad@gmail.com>
2018-05-15 19:26:14 +0800

    jQuery AJAX used to call store method of API

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,39 @@
 jQuery(document).ready(function($) {
-	
+
+	$('.form-shorten').on('click', '#button-shorten', function(event) {
+		event.preventDefault();
+		// Cache selectors
+		var $input_shorten = $('#input-shorten');
+		var $button_shorten = $(this);
+		var $response_shorten = $('#response-shorten');
+
+		// Add loading state to the form
+		$button_shorten.addClass('progress-bar progress-bar-striped progress-bar-animated');
+		$button_shorten.prop('disabled', true)
+		$input_shorten.prop('readonly', true);
+
+		$.ajax({
+			url: 'api/store',
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				longurl: $input_shorten.val()
+			},
+		})
+		.done(function(res) {
+			$input_shorten.val('');
+			$response_shorten.html(res.data.message + '<br>' + res.data.url + '<br>' + res.data.stats).fadeIn();
+		})
+		.fail(function() {
+			$response_shorten.html(res.data.message).fadeIn();
+		})
+		.always(function() {
+			// Remove loading state from the form
+			$button_shorten.removeClass('progress-bar progress-bar-striped progress-bar-animated');
+			$button_shorten.prop('disabled', false)
+			$input_shorten.prop('readonly', false);
+		});
+		
+	});
+
 });
