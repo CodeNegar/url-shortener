5432ce87ad9eb2b461c7dda5417cda990ddf1349
Farhad Ahmadi <ahm.farhad@gmail.com>
2018-05-15 21:21:06 +0800

    Fillable fields set for url model

--- app/url.php
+++ app/url.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class url extends Model
 {
-    //
+    /**
+     * List of allowed fields for filling
+     */
+    protected $fillable = [
+        'url'
+    ];
 }
