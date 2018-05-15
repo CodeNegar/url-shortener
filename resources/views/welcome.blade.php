2c23487283884dcde3c3359e6b43dee41cd63191
Farhad Ahmadi <ahm.farhad@gmail.com>
2018-05-15 19:02:28 +0800

    Placeholder for the API response created

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,9 +19,12 @@
   <body class="text-center">
     <form class="form-shorten">
       <h1 class="h3 mb-3 font-weight-normal">Please Enter your URL</h1>
-      <label for="inputURL" class="sr-only">Long URL</label>
-      <input type="url" id="inputURL" class="form-control mb-3" value="http://" name="longurl" id="longurl" placeholder="Long URL" required autofocus>
-      <button class="btn btn-lg btn-primary btn-block" id="shorten-btn" type="submit">Shorten</button>
+      <label for="input-shorten" class="sr-only">Long URL</label>
+      <input type="url" class="form-control mb-3" placeholder="http://" name="input-shorten" id="input-shorten" placeholder="Long URL" required autofocus>
+      <button class="btn btn-lg btn-primary btn-block mb-3" id="button-shorten" type="submit">Shorten</button>
+      <div id="response-shorten" class="alert alert-primary" role="alert">
+        
+      </div>
       <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
     </form>
 
