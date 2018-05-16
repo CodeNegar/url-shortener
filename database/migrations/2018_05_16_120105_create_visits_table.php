d5798eaa44099fafa99abe0145590570b6f617d9
Farhad Ahmadi <ahm.farhad@gmail.com>
2018-05-16 22:20:37 +0800

    User info can be null as all devices don't provide their details

--- database/migrations/2018_05_16_120105_create_visits_table.php
+++ database/migrations/2018_05_16_120105_create_visits_table.php
@@ -17,14 +17,14 @@ class CreateVisitsTable extends Migration
             $table->increments('id');
             $table->timestamps();
             $table->integer('url_id')->unsigned()->index('url_id');;
-            $table->string('os', 25);
-            $table->string('client_type', 25); // e.g browser, feedreader
-            $table->string('client_name', 25); // e.g Firefox, Greader
-            $table->string('device', 25); // e.g smartphone, desktop
-            $table->string('referrer', 100);
+            $table->string('os', 25)->nullable();;
+            $table->string('client_type', 25)->nullable();; // e.g browser, feedreader
+            $table->string('client_name', 25)->nullable();; // e.g Firefox, Greader
+            $table->string('device', 25)->nullable();; // e.g smartphone, desktop
+            $table->string('referrer', 100)->nullable();;
             $table->string('ip', 32);
-            $table->string('country', 32);
-            $table->string('user_agent', 256);
+            $table->string('country', 32)->nullable();;
+            $table->string('user_agent', 256)->nullable();;
             $table->boolean('is_bot');
         });
     }
