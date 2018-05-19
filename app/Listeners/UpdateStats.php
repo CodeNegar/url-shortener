e20333e72ba4557ef98e38a6ee89f4c4ee2ced39
Farhad Ahmadi <ahm.farhad@gmail.com>
2018-05-19 20:03:11 +0800

    Get client real ip instead of balancer ip

--- app/Listeners/UpdateStats.php
+++ app/Listeners/UpdateStats.php
@@ -36,7 +36,8 @@ class UpdateStats
 
         // Save details of the visit
         $user_agent = $event->request->header('User-Agent');
-        $geo_data = geoip();
+        $ip = $this->get_real_ip();
+        $geo_data = geoip($ip);
         $country = $geo_data->country ?? '';
         $dd = new DeviceDetector($user_agent);
         $dd->parse();
@@ -49,11 +50,24 @@ class UpdateStats
         $visit->client_name = $dd->getClient('name');
         $visit->device = $dd->getDeviceName();
         $visit->referrer = $event->request->server('HTTP_REFERER');
-        $visit->ip = $event->request->ip();
+        $visit->ip = $ip;
         $visit->country = $country;
         $visit->user_agent = $user_agent;
         $visit->is_bot = $dd->isBot();
 
         $visit->save();
+    }
+
+    protected function get_real_ip(){
+        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+            if (array_key_exists($key, $_SERVER) === true){
+                foreach (explode(',', $_SERVER[$key]) as $ip){
+                    $ip = trim($ip);
+                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+                        return $ip;
+                    }
+                }
+            }
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,8 @@ Route::get('/index', 'UrlController@index');
 Route::post('/store', 'UrlController@store');
 Route::get('/{url}/stats', 'UrlController@stats');
 Route::get('/test', function(){
-    var_dump(geoip('118.100.4.100')->country);
     return [
-        'status' => 'success',
-        'data'   => 'API is working correctly'
+        'status'   => 'success',
+        'data'     => 'API is working correctly'
     ];
 });
