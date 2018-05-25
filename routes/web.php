671ce24c91055d3e6859098f855c89ec8a28cb33
Farhad Ahmadi <ahm.farhad@gmail.com>
2018-05-25 23:00:05 +0800

    Route named added for homepage

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
-Route::get('/{url}/go', 'UrlController@show')->name('go');;
+Route::get('/{url}/go', 'UrlController@show')->name('go');
 
 Route::get('/{url}/stats', 'UrlController@stats')->name('stats'); // todo: use view to show charts
