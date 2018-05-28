87fe56b20db8976a24aeb460c6da765fb008dc6c
Farhad Ahmadi <ahm.farhad@gmail.com>
2018-05-28 18:35:42 +0800

    Jquery removed from assets, vue & Axios added

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,5 +1,5 @@
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <script src="https://unpkg.com/vue@2.5.16/dist/vue.js"></script>
     <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
 
   </body>
