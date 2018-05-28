e0f72a84ddfba2d8d79ca4ae72f6d56e5790ff3f
Farhad Ahmadi <ahm.farhad@gmail.com>
2018-05-28 18:35:09 +0800

    Main wrapper was changed to vue app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,8 @@
 @include('parts.header')
 
-    <form class="form-shorten">
-      <h1 class="h3 mb-3 font-weight-normal">Please Enter your URL</h1>
-      <label for="input-shorten" class="sr-only">Long URL</label>
-      <input type="url" class="form-control mb-3" placeholder="http://" name="input-shorten" id="input-shorten" placeholder="Long URL" required autofocus>
-      <button class="btn btn-lg btn-primary btn-block mb-3" id="button-shorten" type="submit">Shorten</button>
-      <div id="response-shorten" class="alert alert-primary" role="alert">
-        
-      </div>
-      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
-    </form>
+<div id="app">
 
+    <url-shortener-form></url-shortener-form>
+
+</div>
 @include('parts.footer')
