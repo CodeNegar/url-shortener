ce07c46bf037970c7640324b406932e2de60ccb0
Farhad Ahmadi <ahm.farhad@gmail.com>
2018-05-31 22:03:27 +0800

    Url factory needs to generate id for each loop

--- database/factories/UrlFactory.php
+++ database/factories/UrlFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Url::class, function (Faker $faker) {
     return [
+        'id' => str_random(),
         'url' => $faker->url,
         'hits' => $faker->numberBetween(0, 99999),
     ];
