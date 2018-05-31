1cd76efcaf089b647b6870fb42838f95892588a5
Farhad Ahmadi <ahm.farhad@gmail.com>
2018-05-31 22:05:02 +0800

    Id field of Url model made fillable and disabled its increment behaviour

--- app/url.php
+++ app/url.php
@@ -7,9 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Url extends Model
 {
     /**
+     * ID field is not an auto increment int
+     */
+    public $incrementing = false;
+
+    /**
      * List of allowed fields for filling
      */
     protected $fillable = [
+        'id',
         'url'
     ];
 
